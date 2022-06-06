c1e74040f86ee7826eb5867b76c7c05fec4e44b4
Hyun Sung Cho <hcho774@gmail.com>
2022-06-05 22:25:51 -0400

    commit tree dd498f267fc8959a9917a8b488884e0b3a5a0463

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -3,4 +3,49 @@ class ApplicationController < Sinatra::Base
   
   # Add routes
   
+  get '/bootcamps' do
+    bootcamp = Bootcamp.all
+    bootcamp.to_json
+  end 
+
+    
+  get '/bootcamps/:id' do
+    bootcamp = Bootcamp.find(params[:id])
+    bootcamp.to_json(include: :reviews)
+  end
+
+  
+
+
+
+
+  get '/messages' do
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json
+end 
+
+  post '/messages' do
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+
+  end
+
+
 end
